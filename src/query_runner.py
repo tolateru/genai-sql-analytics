838c9eab5bb750eb73c106822e6e30b1bf8d9b7e
tolateru <tolateru0813@gmail.com>
2026-04-22 10:44:28 -0400

    Included a provider csv path for join

--- src/query_runner.py
+++ src/query_runner.py
@@ -2,15 +2,23 @@ import duckdb
 import pandas as pd
 import os
 
-def run_sql_on_csv(sql: str, csv_path: str) -> pd.DataFrame:
+def run_sql_on_csv(sql: str, claims_csv_path: str, provider_csv_path: str) -> pd.DataFrame:
     con = duckdb.connect()
 
-    # Register the CSV as the table your SQL expects
+    claims_csv_path = claims_csv_path.replace("\\", "/")
+    provider_csv_path = provider_csv_path.replace("\\", "/")
+
     con.execute(f"""
         CREATE OR REPLACE VIEW healthcare_claims AS
-        SELECT * FROM read_csv_auto('{csv_path}', HEADER=TRUE);
+        SELECT * FROM read_csv_auto('{claims_csv_path}', HEADER=TRUE);
+    """)
+
+    con.execute(f"""
+        CREATE OR REPLACE VIEW provider_reference AS
+        SELECT * FROM read_csv_auto('{provider_csv_path}', HEADER=TRUE);
     """)
 
     df = con.execute(sql).df()
     con.close()
+
     return df
